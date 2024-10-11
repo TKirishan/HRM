3158b2e698c9a5af74c480541b70c1fe686b7fc9
TKirishan <134942371+TKirishan@users.noreply.github.com>
2024-10-11 09:07:44 +0530

    2nd Updated Attendance.js after QA

--- src/Pages/Attendance.js
+++ src/Pages/Attendance.js
@@ -2,24 +2,24 @@ import React from 'react';
 import { Card, Nav, Tab } from 'react-bootstrap';
 import DailyLog from './Att_DailyLog';
 import AttendanceDetails from './AttendanceDetails';
-import RequestPage from './Request';
+
 import Summary from './Summary';
 
 const Attendance = () => {
     return (
-        <Tab.Container defaultActiveKey="Att_DailyLog"> {/* Set defaultActiveKey to "Att_DailyLog" */}
+        <Tab.Container defaultActiveKey="Att_DailyLog"> {/* Ensure that "Att_DailyLog" is correct */}
             <Card>
                 {/* Horizontal Navbar in Card.Header */}
                 <Card.Header>
                     <Nav variant="tabs">
                         <Nav.Item>
-                            <Nav.Link eventKey="Att_DailyLog">Daily Log</Nav.Link>
+                            <Nav.Link eventKey="Att_DailyLog">Daily Log</Nav.Link> {/* No typo in eventKey */}
                         </Nav.Item>
+                        
                         <Nav.Item>
-                            <Nav.Link eventKey="Request">Request</Nav.Link>
-                        </Nav.Item> <Nav.Item>
                             <Nav.Link eventKey="AttendanceDetails">Attendance Details</Nav.Link>
                         </Nav.Item>
+                        
                         <Nav.Item>
                             <Nav.Link eventKey="Summary">Summary</Nav.Link>
                         </Nav.Item>
@@ -30,22 +30,17 @@ const Attendance = () => {
                     <Tab.Content>
                         {/* Daily Log Content */}
                         <Tab.Pane eventKey="Att_DailyLog">
-                            <DailyLog />
+                            <DailyLog /> {/* Ensure the DailyLog component is defined correctly */}
                         </Tab.Pane>
-
-                        {/* Request Content */}
-                        <Tab.Pane eventKey="Request">
-                            <RequestPage /> {/* Include the RequestPage component */}
-                        </Tab.Pane>
-
-                          {/* Attendance Details Content */}
-                          <Tab.Pane eventKey="AttendanceDetails">
-                            <AttendanceDetails /> {/* Include the Attendance Details Page component */}
+                        
+                        {/* Attendance Details Content */}
+                        <Tab.Pane eventKey="AttendanceDetails">
+                            <AttendanceDetails /> {/* Ensure the AttendanceDetails component is defined correctly */}
                         </Tab.Pane>
 
                         {/* Summary Content */}
                         <Tab.Pane eventKey="Summary">
-                            <Summary />  {/* Include the Summary component */}
+                            <Summary />  {/* Ensure the Summary component is defined correctly */}
                         </Tab.Pane>
                     </Tab.Content>
                 </Card.Body>
